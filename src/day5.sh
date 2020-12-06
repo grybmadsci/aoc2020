d99a09b88e476a6242fb97a7a8cf9b091198ffdc
madsci <jph1996@gmail.com>
2020-12-06 04:50:49 -0800

    add day 5 part 2

--- src/day5.sh
+++ src/day5.sh
@@ -1,7 +1,21 @@
 #!/bin/bash -e
 
+TMP=`mktemp`
+trap "rm -rf $TMP" EXIT
+
 # Part 1, this problem is just a weird binary encoding where 0's and 1's are F/L and R/B
 # respectively. Translate, convert from binary to decimal, then sort | head to find the max.
-tr "BFRL" "1010" | (while read BIN; do echo $((2#$BIN)); done) | sort -nr | head -n1
+tr "BFRL" "1010" | (while read BIN; do echo $((2#$BIN)); done) > $TMP
+sort -nr $TMP | head -n1
 
 
+# Similar, but checking the higher-order bits for all-0 or all-1 and checking for presence of the
+# seat "id" +/- 1 in the output of Part 1.
+seq 1023 | while read SEAT
+do
+  if grep -qx $SEAT $TMP; then continue; fi
+  if test $(($SEAT & 0x3f8)) -eq 0 -o $(($SEAT & 0x3f8)) -eq 1026; then continue; fi
+  if grep -qx $((SEAT + 1)) $TMP && grep -qx $((SEAT - 1)) $TMP; then
+    echo $SEAT
+  fi
+done
